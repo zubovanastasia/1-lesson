c13cfc66da90724689495b8fef2845cd7f0e69f6
Anastasiia <zubovanastasiaa@mail.ru>
2021-09-05 21:17:15 +0300

    1

--- 1 lesson/ViewController.swift
+++ 1 lesson/ViewController.swift
@@ -1,19 +1,74 @@
 //
 //  ViewController.swift
-//  1 lesson
+//  1 урок
 //
-//  Created by Anastasiia Zubova on 05.09.2021.
+//  Created by Anastasiia Zubova on 03.09.2021.
 //
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var ScrollView: UIScrollView!
+    @IBOutlet private weak var LoginText: UITextField!
+    @IBOutlet private weak var PassText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addGesture()
+    
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.addObservers()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.removeObservers()
+    }
+    private func addGesture() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(handleGesture))
+        self.ScrollView.addGestureRecognizer(gesture)
+    }
+    @objc private func handleGesture() {
+        self.ScrollView.endEditing(true)
+    }
+    private func addObservers () {
+        NotificationCenter.default
+            .addObserver(self, selector: #selector(handleKeyboardWillShow) , name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default
+            .addObserver(self, selector: #selector(handleKeyboardWillHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+    }
+    
+    private func removeObservers () {
+        NotificationCenter.default
+            .removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default
+            .removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc private func handleKeyboardWillShow () {
+        self.ScrollView.contentInset.bottom += 150
+    }
+    @objc private func handleKeyboardWillHide () {
+        self.ScrollView.contentInset.bottom = 0
     }
 
-
+    
+    @IBAction private func Button(_ sender: Any) {
+    let login = self.LoginText.text
+        let pass = self.PassText.text
+        
+        if login == "admin" && pass == "123456" {
+            print ("Успешная авторизация")
+        } else {
+            print("Не авторизованы")
+        }
+    }
+    
 }
 
