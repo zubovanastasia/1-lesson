8f7b9cea4498a5a95038d1b4d2fa68429c0df38d
Anastasiia <zubovanastasiaa@mail.ru>
2021-09-27 00:56:16 +0300

    6

--- 1 lesson/TabsTable/CommunitiesController.swift
+++ 1 lesson/TabsTable/CommunitiesController.swift
@@ -11,9 +11,13 @@ class CommunitiesController: UITableViewController {
     
     var nameCom = ["Искусство", "Программирование", "Музыка", "Технологии", "Наука"]
     var image = ["1","2","3","4","5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.title = "Мои Сообщества"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -23,13 +27,12 @@ class CommunitiesController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "community", for: indexPath) as! CommunitiesCell
         let name = nameCom[indexPath.row]
+        let name2 = image[indexPath.row]
+        cell.imageCommunity.image = UIImage(named: name2)
         cell.nameCommunity.text = name
         return cell
 }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let cell = tableView.cellForRow(at: indexPath) as! CommunitiesCell
-        let name = image[indexPath.row]
-        cell.imageCommunity.image = UIImage(named: name)
     }
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
