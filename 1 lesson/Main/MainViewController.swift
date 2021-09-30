8f80ad40cfa5a4c9274e84293b2f32708e642d5a
Anastasiia <zubovanastasiaa@mail.ru>
2021-09-30 17:28:32 +0300

    7

--- 1 lesson/Main/MainViewController.swift
+++ 1 lesson/Main/MainViewController.swift
@@ -6,12 +6,67 @@ class ViewController: UIViewController {
     @IBOutlet private weak var ScrollView: UIScrollView!
     @IBOutlet private weak var LoginText: UITextField!
     @IBOutlet private weak var PassText: UITextField!
+    @IBOutlet weak var textPass: UILabel!
+    @IBOutlet weak var textLogin: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var vk: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addGesture()
-    
+        animateTitleAppearing()
+        animateTitlesAppearing()
+        animateAuthButton()
+        animateFieldsAppearing()
     }
+    
+    func animateTitlesAppearing() {
+        let offset = view.bounds.width
+        textLogin.transform = CGAffineTransform(translationX: -offset, y: 0)
+        textPass.transform = CGAffineTransform(translationX: offset, y: 0)
+        UIView.animate(withDuration: 1, delay: 1, options: .curveEaseOut, animations: {
+            self.textLogin.transform = .identity
+            self.textPass.transform = .identity
+            
+        },
+                       completion: nil)
+    }
+    
+    func animateTitleAppearing() {
+        self.vk.transform = CGAffineTransform(translationX: 0, y: -self.view.bounds.height/2)
+        
+        UIView.animate(withDuration: 1, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+            self.vk.transform = .identity
+                       },
+                       completion: nil)
+    }
+    
+    func animateFieldsAppearing() {
+        let fadeInAnimation = CABasicAnimation(keyPath: "opacity")
+        fadeInAnimation.fromValue = 0
+        fadeInAnimation.toValue = 1
+        fadeInAnimation.duration = 1
+        fadeInAnimation.beginTime = CACurrentMediaTime() + 1
+        fadeInAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        fadeInAnimation.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.textLogin.layer.add(fadeInAnimation, forKey: nil)
+        self.textPass.layer.add(fadeInAnimation, forKey: nil)
+    }
+    
+    func animateAuthButton() {
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.stiffness = 200
+        animation.mass = 2
+        animation.duration = 2
+        animation.beginTime = CACurrentMediaTime() + 1
+        animation.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.button.layer.add(animation, forKey: nil)
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.addObservers()
