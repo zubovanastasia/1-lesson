876f7ba5986d5db0a719181fd540ab0aa903f563
Anastasiia <zubovanastasiaa@mail.ru>
2021-10-07 23:24:38 +0300

    9

--- 1 lesson/TabsTable/CommunitiesController.swift
+++ 1 lesson/TabsTable/CommunitiesController.swift
@@ -42,3 +42,17 @@ class CommunitiesController: UITableViewController {
       
     }
 }
+class CommunitiesNavigationController: UINavigationController, UINavigationControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if operation == .push {
+            return NavigationPushAnimator()
+        } else if operation == .pop {
+            return NavigationPopAnimator()
+        }
+        return nil
+    }
+}
