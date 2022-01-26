be054682ea8cf4bb05135f93af65d92e0fd405fe
Anastasiia <zubovanastasiaa@mail.ru>
2022-01-26 23:19:46 +0300

    Proxy

--- 1 lesson/Services/Session.swift
+++ 1 lesson/Services/Session.swift
@@ -14,6 +14,7 @@ import Firebase
     var token: String = ""
     var userId: String = ""
     var ref: DatabaseReference?
+    var accessGranted = false
     
     init (userId: String) {
         self.ref = nil
